32f6ded0f18405f80fc6c5a2a89037eea256cb70
lilliputten <lilliputten@yandex.ru>
2023-03-02 16:06:38 +0700

    Build publish.0.0.2, 2023.03.02, 00:01 (230302-0001)

--- _next/static/chunks/pages/404-5929ae228d450de9.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{6141:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return u(680)}])},680:function(n,t,u){"use strict";u.r(t),u.d(t,{default:function(){return o}});var e=u(5893),i=u(6443),r=u(7725),_=u(6704),c=u(1432);function o(){var n=i.notFoundTitle,t=(0,_.dV)(n);return(0,e.jsx)(r.wd,{title:t,children:(0,e.jsx)(c.NZ,{})})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=6141)}),_N_E=n.O()}]);
-//# sourceMappingURL=404-5929ae228d450de9.js.map
